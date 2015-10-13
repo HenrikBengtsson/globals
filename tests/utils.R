844bca9ec697caa6156465e348e1ed6d71165927
hb <hb@aroma-project.org>
2015-10-13 16:31:33 -0700

    Add internal where() [#9]

--- tests/utils.R
+++ tests/utils.R
@@ -5,13 +5,16 @@ findBasePkgs <- globals:::findBasePkgs
 isBasePkgs <- globals:::isBasePkgs
 is.base <- globals:::is.base
 is.internal <- globals:::is.internal
+where <- globals:::where
 
+
+message("* asFunction() ...")
 fcn <- asFunction({ 1 })
 print(fcn())
 stopifnot(fcn() == 1)
 
 
-## Base R packages
+message("* findBasePkgs() & isBasePkgs() ...")
 basePkgs <- findBasePkgs()
 print(basePkgs)
 stopifnot(length(basePkgs) > 1L)
@@ -20,10 +23,52 @@ for (pkg in basePkgs) {
 }
 stopifnot(!isBasePkgs("globals"))
 
+
+message("* is.base() & is.internal() ...")
 stopifnot(is.base(base::library))
 stopifnot(!is.base(globals::globalsOf))
 stopifnot(is.internal(print.default))
 stopifnot(!is.internal(globals::globalsOf))
 
 
+
+
+message("* where() ...")
+
+message("- where('sample') ...")
+env <- where("sample", mode="function")
+print(env)
+stopifnot(identical(env, baseenv()))
+
+
+message("- where('sample', mode='integer') ...")
+env <- where("sample", mode="integer")
+print(env)
+stopifnot(is.null(env))
+
+
+message("- where('sample2') ...")
+sample2 <- base::sample
+env <- where("sample2", mode="function")
+print(env)
+stopifnot(identical(env, environment()))
+
+
+message("- where() - local objects of functions ...")
+aa <- 1
+
+foo <- function() {
+  bb <- 2
+  list(aa=where("aa"), bb=where("bb"), cc=where("cc"), envir=environment())
+}
+
+envs <- foo()
+str(envs)
+stopifnot(identical(envs$aa, globalenv()))
+stopifnot(identical(envs$bb, envs$envir))
+stopifnot(is.null(envs$cc))
+
+rm(list=c("aa", "envs", "foo", "where"))
+
+message("* where() ... DONE")
 
