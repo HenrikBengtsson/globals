e34873146efefb0bcf0f9ebee58c8a84dbe323bd
hb <hb@aroma-project.org>
2015-10-13 17:35:54 -0700

    Proper subsetting of 'where' with [() and cleanup() [#9]

--- R/cleanup.R
+++ R/cleanup.R
@@ -10,25 +10,34 @@ cleanup <- function(...) UseMethod("cleanup")
 #' @aliases cleanup
 #' @export
 cleanup.Globals <- function(globals, drop=c("primitives", "internals", "base-packages"), ...) {
+  keep <- rep(TRUE, times=length(globals))
+  names(keep) <- names(globals)
+
   ## Drop objects that are primitive functions
   if ("primitives" %in% drop) {
     for (name in names(globals)) {
-      if (is.primitive(globals[[name]])) globals[[name]] <- NULL
+      if (is.primitive(globals[[name]])) keep[name] <- FALSE
     }
   }
 
   ## Drop objects that calls .Internal()
   if ("internals" %in% drop) {
     for (name in names(globals)) {
-      if (is.internal(globals[[name]])) globals[[name]] <- NULL
+      if (is.internal(globals[[name]])) keep[name] <- FALSE
     }
   }
 
   ## Drop objects that are part of one of the "base" packages
   if ("base-packages" %in% drop) {
     for (name in names(globals)) {
-      if (is.base(globals[[name]])) globals[[name]] <- NULL
+      if (is.base(globals[[name]])) keep[name] <- FALSE
     }
+  }
+
+  if (!all(keep)) {
+    where <- attr(globals, "where")
+    globals <- globals[keep]
+    attr(globals, "where") <- where[keep]
   }
 
   globals
