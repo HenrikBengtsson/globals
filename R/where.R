6020f259fb3bc6b91420d79084318674fdd4fc3a
hb <hb@aroma-project.org>
2017-03-08 21:47:41 -0800

    Slightly less verbose output

--- R/where.R
+++ R/where.R
@@ -12,8 +12,7 @@ where <- function(x, where=-1, envir=if (missing(frame)) { if (where < 0) parent
   
   ## Search
   while (!identical(envir, emptyenv())) {
-    mdebug("- searching location: %s", sQuote(envname(envir)))
-    mdebug("- content of %s: %s", sQuote(envname(envir)), hpaste(sQuote(ls(envir = envir, all.names = TRUE))))
+    mdebug("- searching %s: %s", sQuote(envname(envir)), hpaste(sQuote(ls(envir = envir, all.names = TRUE))))
     if (exists(x, envir=envir, mode=mode, inherits=FALSE)) {
       mdebug("  + found in location: %s", sQuote(envname(envir)))
       mdebug("where(%s, where = %d, envir = %s, mode = %s, inherits = %s) ... DONE", sQuote(x), where, sQuote(envname(envir)), sQuote(mode), inherits)
