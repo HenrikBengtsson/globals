ed12847c2a8ba9b2392a71caa3ad48ab54463324
DavisVaughan <davis@rstudio.com>
2020-08-04 20:16:48 -0400

    Invert order of cleanup loop

--- R/cleanup.R
+++ R/cleanup.R
@@ -18,30 +18,42 @@ cleanup.Globals <- function(globals, drop = c("missing", "base-packages"),
   names(keep) <- names
 
   ## Drop non-found objects
-  if ("missing" %in% drop) {
-    for (name in names) {
-      if (is.null(where[[name]])) keep[name] <- FALSE
-    }
-  }
+  drop_missing <- "missing" %in% drop
 
   ## Drop objects that are part of one of the "base" packages
-  if ("base-packages" %in% drop) {
-    for (name in names) {
-      if (is_base_pkg(environmentName(where[[name]]))) keep[name] <- FALSE
-    }
-  }
+  drop_base <- "base-packages" %in% drop
 
   ## Drop objects that are primitive functions
-  if ("primitives" %in% drop) {
-    for (name in names) {
-      if (is.primitive(globals[[name]])) keep[name] <- FALSE
-    }
-  }
+  drop_primitives <- "primitives" %in% drop
 
   ## Drop objects that calls .Internal()
-  if ("internals" %in% drop) {
-    for (name in names) {
-      if (is_internal(globals[[name]])) keep[name] <- FALSE
+  drop_internals <- "internals" %in% drop
+
+  for (name in names) {
+    env <- where[[name]]
+
+    if (drop_missing && is.null(env)) {
+      keep[[name]] <- FALSE
+      next
+    }
+
+    env_name <- environmentName(env)
+
+    if (drop_base && is_base_pkg(env_name)) {
+      keep[[name]] <- FALSE
+      next
+    }
+
+    global <- globals[[name]]
+
+    if (drop_primitives && is.primitive(global)) {
+      keep[[name]] <- FALSE
+      next
+    }
+
+    if (drop_internals && is_internal(global)) {
+      keep[[name]] <- FALSE
+      next
     }
   }
 
