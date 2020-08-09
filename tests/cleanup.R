d25a068f51b2ac16a57aae78f24624afea4a6bcc
Henrik Bengtsson <hb@aroma-project.org>
2020-08-09 13:28:58 -0700

    TESTS: Add true-negative case too

--- tests/cleanup.R
+++ tests/cleanup.R
@@ -4,15 +4,17 @@ message("*** cleanup() ...")
 
 ## Don't clean out renamed base functions
 globals <- as.Globals(list(
+  my_fcn      = function(x) x,
   identity    = base::identity,
   my_identity = base::identity   ## should not be deleted
 ))
 
-stopifnot(all(c("identity", "my_identity") %in% names(globals)))
+stopifnot(all(c("my_fcn", "identity", "my_identity") %in% names(globals)))
 
 globals2 <- cleanup(globals)
 stopifnot(
-  !"identity" %in% names(globals2),
+       "my_fcn" %in% names(globals2),
+    !"identity" %in% names(globals2),
   "my_identity" %in% names(globals2)
 )
 
