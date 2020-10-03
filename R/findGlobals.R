f8150bef7f8f67e81243a85ef44f6bfde34ffa4d
Henrik Bengtsson <hb@aroma-project.org>
2020-10-03 14:29:38 -0700

    Work around bug #64 also for older versions of R-devel

--- R/findGlobals.R
+++ R/findGlobals.R
@@ -438,7 +438,8 @@ make_usage_collector <- local({
   ## https://bugs.r-project.org/bugzilla/show_bug.cgi?id=17935 bug in the
   ## stats:::`[.formula` function
   ## See also: https://github.com/HenrikBengtsson/globals/issues/64
-  if (getRversion() < "4.1.0") {
+  if (getRversion() < "4.1.0" || is.null(ver <- R.version$`svn rev`) ||
+      is.na(ver <- as.integer(ver)) || ver < 79288) {
     ## Local copy of codetools:::collectUsageCall()
     .collectUsageCall <- NULL
 
