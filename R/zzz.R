ad2cb3c4b6168bf7c7b4b1edf2396292b78fa6fb
Henrik Bengtsson <hb@aroma-project.org>
2022-05-06 16:49:05 -0700

    PROTOTYPING: Make it possible to return globalenv() instead of emptyenv()
    Related to https://github.com/HenrikBengtsson/future/issues/515

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,11 @@
 ## covr: skip=all
 .onLoad <- function(libname, pkgname) {
+  update_package_option("globals.debug", mode = "logical")
+  debug <- getOption("globals.debug", FALSE)
+
+  ## Set future options based on environment variables
+  update_package_options(debug = debug)
+
   ## Memoize: Already here, when the package is loaded, record whether
   ## some packages are 'base' packages or not.
   ## Packages that most likely are 'base' packages:
