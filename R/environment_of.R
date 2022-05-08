14ccd19d1f402ca2b9ace06d8b612d2161eb6506
Henrik Bengtsson <hb@aroma-project.org>
2022-05-07 21:24:06 -0700

    CLEANUP: Remove temporary, internal option 'globals.environment_of.default' that was used while prototyping in the 'develop' branch

--- R/environment_of.R
+++ R/environment_of.R
@@ -1,9 +1,9 @@
 # A safe version of base::environment() that returns emptyenv()
 # if NULL is passed, instead of the calling environment.
 # Related to https://github.com/HenrikBengtsson/globals/issues/79
-environment_of <- function(obj, default = getOption("globals.environment_of.default", emptyenv())) {
-  if (is.null(obj)) return(default)
+environment_of <- function(obj) {
+  if (is.null(obj)) return(emptyenv())
   e <- environment(obj)
-  if (is.null(e)) return(default)
+  if (is.null(e)) return(emptyenv())
   e
 }
